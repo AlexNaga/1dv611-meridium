135a069483ad4fe8464ce35013b6afc1b9b037cd
fridamelin <fm222gn@student.lnu.se>
2018-03-29 14:34:39 +0200

    implementerat arkiveringen i express

--- app.js
+++ app.js
@@ -24,11 +24,10 @@ app.post('/url', function(req, res) {
     if (validEmail.validate(email) === false) return res.send('Invalid email!');
 
     let id = crypto.randomBytes(10).toString('hex');
-    let download = require('./download.js');
-    download(url, id, function(error) {
+    let download = require('./archive.js');
+    download(id, url, function(error) {
         if (error) {
-            console.log(error);
-            // return res.status(500).send('Could not archive the desired url.');
+            return console.log(error);
         }
 
         let downloadUrl = baseUrl + '/archives/' + id + '.zip';
@@ -41,35 +40,18 @@ app.post('/url', function(req, res) {
         }
         sendMail(settings, function(error, info) {
             if (error) {
-                console.log(error);
-                // return res.status(500).send('Could not send email.');
+                return console.log(error);
             }
 
             console.log('Message sent: %s', info.messageId);
         });
     });
-    // request.post('http://localhost:3500/wrapper', {form:{url: url, webhookUrl: webhookUrl, id: id}});
     res.status(200).send('Thank you!<br>We are now downloading ' + url + '<br>When complete we will send email to ' + email + '<br><a href="/">Go back</a>');
 });
 
-// app.post('/wrapper', function (req, res) {
-//     request.post(baseUrl + '/callback', {form:{url: req.body.url, email: req.body.email, id: '12357'}});
-// });
-
-// app.post('/callback/:id', function(req, res) {
-//     let id = req.param.id;
-//     let url = req.param.zipFile;
-//     let downloadUrl = baseUrl + '/archives/' + id + '.zip';
-
-//     let sendMail = require('./sendMail.js');
-//     let subject = 'Your download is complete ✔';
-//     let html = '<p><b>Your download of ' + url + ' is complete!</b></p><p><a href="' + downloadUrl + '">Download zip</a></p>';
-//     sendMail(email, subject, html);
-// });
-
 app.get('/archives/:id', function(req, res) {
     let id = req.params.id;
-    res.status(200).sendFile(path.join(__dirname + '/' + id));
+    res.status(200).sendFile(path.join(__dirname + '/archive/' + id));
 });
 
 app.listen(3500, function() {
