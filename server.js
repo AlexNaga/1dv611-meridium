eb61aa7ef216543363219aa38175bba5c467194b
Robin Nowakowski <bobinush@gmail.com>
2018-04-09 14:52:43 +0200

    hostname http server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
+const hostname = process.env.HOSTNAME || 'localhost';
 const server = http.createServer(app);
-server.listen(port);
-
-console.log('The server is now running at: http://localhost:' + port);
+server.listen(port, hostname, () => {
+    console.log(`The server is now running at: http://${hostname}:${port}`);
+});
