ac8ed30234e15027ae85f54aaea0fbaaf409a75e
fridamelin <fm222gn@student.lnu.se>
2018-04-19 11:25:41 +0200

    Changed names on some titles

--- src/routes/schedule.js
+++ src/routes/schedule.js
@@ -3,7 +3,6 @@ const ScheduleController = require('../controllers/schedulejobsController');
 
 module.exports = (app, baseRoute) => {
     // Views
-    
     app.get(baseRoute + '/edit/:id', ScheduleController.getSchedule);
 
     // Lists all schedules
