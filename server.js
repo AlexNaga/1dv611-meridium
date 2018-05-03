1047cab593bcfe7ec2818642e2d95e38429d7bfd
AlexNaga <an222zd@student.lnu.se>
2018-05-03 16:19:35 +0200

    Set a default port number

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv').load();
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3500;
 const hostname = 'localhost';
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
