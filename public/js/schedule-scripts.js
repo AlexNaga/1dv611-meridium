8039f6abea6fbd84f202cf376d597efa58e27efa
Robin Nowakowski <bobinush@gmail.com>
2018-05-07 15:06:48 +0200

    responsive + err msg

--- public/js/schedule-scripts.js
+++ public/js/schedule-scripts.js
@@ -10,32 +10,23 @@ function setupPauseListener() {
             button.addEventListener('click', (e) => {
                 let icons = e.currentTarget.children[0];
                 let id = e.currentTarget.getAttribute('data-id');
-                toggleLoadingSpinner(icons);
+                button.classList.toggle('is-loading');
                 fetchUrl('/schedules/pause/' + id, { method: 'POST' })
                     .then((data) => {
                         if (data.success) {
                             for (let b of pausePlayButtons) {
                                 b.classList.toggle('hidden'); // switch buttons
                             }
-                            toggleLoadingSpinner(icons);
                         } else {
                             toggleError(icons);
+                            flashMessage(data.message);
                         }
+                        button.classList.toggle('is-loading');
                     });
             });
         }
     }
 }
-
-function toggleLoadingSpinner(target) {
-    if (!target.parentElement.classList.contains('is-danger')) {
-        target.children[0].classList.toggle('hidden');      // toggle pause/play icon
-    }
-    target.children[2].classList.add('hidden');             // hide exclamation icon
-    target.parentElement.classList.remove('is-danger');     // remove red button color
-    target.children[1].classList.toggle('hidden');          // toggle spinner
-}
-
 function toggleError(target) {
     target.parentElement.classList.add('is-danger');    // show red button color
     target.children[2].classList.remove('hidden');      // show exclamation icon
