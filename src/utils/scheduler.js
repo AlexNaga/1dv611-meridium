100664c8ee760c1a718cd21a46cb3fd0426991ac
Robin Nowakowski <bobinush@gmail.com>
2018-05-21 12:05:25 +0200

    Small css changes Scheduler more informative

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -1,32 +1,39 @@
 const httrackWrapper = require('../models/httrackWrapper');
 const validateHttrackSettings = require('./validateHttrackSettings');
-const EmailModel = require('../models/emailModel');
 const Schedules = require('../models/schedules');
-const Archive = require('../models/archive');
 const nodeSchedule = require('node-schedule');
 const Schedule = require('../models/enums').schedule;
 
-exports.nodeSchedule = nodeSchedule.scheduleJob('00 00 03 * * *', async () => {
+/**
+ * Run every day at 03 in the morning
+ * RecurrenceRule (https://github.com/node-schedule/node-schedule#recurrence-rule-scheduling)
+ * second (0-59)
+ * minute (0-59)
+ * hour (0-23)
+ * date (1-31)
+ * month (0-11)
+ * year
+ * dayOfWeek (0-6) Starting with Sunday
+ */
+
+var rule = new nodeSchedule.RecurrenceRule();
+rule.hour = 3;
+
+exports.nodeSchedule = nodeSchedule.scheduleJob(rule, async () => {
     try {
         let schedules = await Schedules.find({ isPaused: false }).exec();
-
-        let everyDay = schedules.filter(s => s.typeOfSchedule === Schedule.DAILY);
-        let everyWeek = schedules.filter(s => s.typeOfSchedule === Schedule.WEEKLY);
-        let everyMonth = schedules.filter(s => s.typeOfSchedule === Schedule.MONTHLY);
-
-        let shouldBeArchived = everyDay;
-
+        let shouldBeArchived = schedules.filter(s => s.typeOfSchedule === Schedule.DAILY);
         let today = new Date().getDay();
         // Run weekly schedules on mondays
         if (today === 1) {
-            shouldBeArchived.push(...everyWeek);
+            shouldBeArchived.push(...schedules.filter(s => s.typeOfSchedule === Schedule.WEEKLY));
 
             let d = new Date();
             let yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
             let weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
             // Run monthly schedules on the last week of the months
             if (weekNo % 4 === 0) {
-                shouldBeArchived.push(...everyMonth);
+                shouldBeArchived.push(...schedules.filter(s => s.typeOfSchedule === Schedule.MONTHLY));
             }
         }
 
